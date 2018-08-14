3676310f2dd8ff7c86b42a68a658c6f7801395ba
nattakit325 <nayangklukcity@gmil.com>
2018-08-14 17:53:46 +0700

    d

--- lesser/connect.php
+++ lesser/connect.php
@@ -1,8 +1,8 @@
 <?php
-    $server="localhost";
-    $user="root";
-    $password="";
-    $db="smartfamer";
+    $server="103.233.192.204";
+    $user="nattakit";
+    $password="xR91c20dLt";
+    $db="nattakit_db";
 
 
     $objCon = mysqli_connect($server,$user,$password,$db);
