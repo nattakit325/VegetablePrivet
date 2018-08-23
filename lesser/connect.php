8874aa5d5129efcb214342a4698fb53e00e7cedd
nattakit325 <nayangklukcity@gmil.com>
2018-08-23 22:46:53 +0700

    ป

--- lesser/connect.php
+++ lesser/connect.php
@@ -6,12 +6,9 @@
 
 
     $objCon = mysqli_connect($server,$user,$password,$db);
-<<<<<<< HEAD
     if($objCon){
         
     }
-=======
->>>>>>> 3fe9053e2bfade09caf1f6127dd5d86a04fa345a
     mysqli_set_charset($objCon,"utf8");
 
 
