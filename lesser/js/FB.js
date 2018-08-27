efbe7d98c30aeb4080bb60f4a4d788952973a98c
nattakit325 <nayangklukcity@gmil.com>
2018-08-27 23:20:23 +0700

    f

--- lesser/js/FB.js
+++ lesser/js/FB.js
@@ -64,3 +64,19 @@ function checkLoginState() {
     statusChangeCallback(response);
   });
 }
+
+function Login(params) {
+    var httpc = new XMLHttpRequest(); // simplified for clarity
+    var url = "get_data.php";
+    httpc.open("POST", url, true); // sending as POST
+
+    httpc.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    httpc.setRequestHeader("Content-Length", params.length); // POST request MUST have a Content-Length header (as per HTTP/1.1)
+
+    httpc.onreadystatechange = function() { //Call a function when the state changes.
+    if(httpc.readyState == 4 && httpc.status == 200) { // complete and no errors
+        alert(httpc.responseText); // some processing here, or whatever you want to do with the response
+        }
+    }
+    httpc.send(params);
+}
