49a6f3167b121a65b749ebfd9a5ca4447e43188e
nattakit415 <nayangklukcity@gmail.com>
2018-08-29 17:57:37 +0700

    chat

--- lesser/ProductDetail.php
+++ lesser/ProductDetail.php
@@ -193,31 +193,20 @@ div#messagesDiv{
         <div class="modal-body">
           <center>
           	<div id="messagesDiv">
-<div class="left_box_chat">gfhgfhfgh</div>
-<div class="right_box_chat">fgfhgfgf</div>
+
 </div>
 <div class="bg-info" style="width:100%;padding:5px 0px;">
 <div class="row">
   
   <div class="col-xs-12">
 <!--  input hidden สำหรับ เก็บ chat_id ล่าสุดที่แสดง-->
-<form>
-  <input name="h_maxID" type="hidden" id="h_maxID" value="0">
-  <input name="userID" type="hidden" id="1" value="0">
-  <input name="userID2" id="userID2" value="<?php echo $objResult["Ownusername"]; ?>" type="hidden">
-  <?php
-  if(empty($_SESSION["username"])){ ?>
-  	<input name="userID1" id="userID1" value="NotMember" type="hidden">
-  <?php }else{
-   ?>
-  <input name="userID1" id="userID1" value="<?php echo $_SESSION["username"]; ?>" type="hidden">
-<?php } ?>
-  <input type="text" class="form-control" name="msg" id="msg" placeholder="Message" >
-  <br>
-  <button type="button" id="myBtn" class="btn btn-primary" onclick="send(document.getElementById('msg').value,document.getElementById('userID1').value,document.getElementById('userID2').value)">ส่งข้อความ</button>
-  <br>
-  </form>
+<input name="userID1" type="hidden" id="userID1" value="<?php echo $_SESSION["username"] ?>">
+  <input name="userID2" type="hidden" id="userID2" value="<?php echo $objResult["username"]; ?>">
+  <!--  input hidden สำหรับ เก็บ chat_id ล่าสุดที่แสดง-->
+  <input name="h_maxID" type="hidden" id="h_maxID" value="0" >
+  <input type="text" class="form-control" name="msg" id="msg" placeholder="Message">
   </div>
+
 </div>
 </div>
 
@@ -506,26 +495,63 @@ function ShowMarker(){
 
 
 <!--------chat --->
-<script>
-
-function send(msg,user1,user2){
-	if (window.XMLHttpRequest) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-  } else { // code for IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xmlhttp.onreadystatechange=function() {
-    if (this.readyState==4 && this.status==200) {
-      document.getElementById("chat").innerHTML=this.responseText;
-      
-      this.responseText = "";
-    }
-  }
-  xmlhttp.open("GET","chat.php?msg="+msg+"&user1="+user1+"user2="+user2,true);
-  xmlhttp.send();
-
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>      
+<script type="text/javascript">
+var load_chat; // กำหนดตัวแปร สำหรับเป็นฟังก์ชั่นเรียกข้อมูลมาแสดง
+var first_load=1; // กำหนดตัวแปรสำหรับโหลดข้อมูลครั้งแรกให้เท่ากับ 1
+load_chat = function(userID){
+	var maxID = $("#h_maxID").val(); // chat_id ล่าสุดที่แสดง
+	$.post("ajax_chat.php",{
+		viewData:first_load,
+		userID:userID,
+		maxID:maxID
+	},function(data){
+		if(first_load==1){ // ถ้าเป็นการโหลดครั้งแรก ให้ดึงข้อมูลทั้งหมดที่เคยบันทึกมาแสดง
+			for(var k=0;k<data.length;k++){ // วนลูปแสดงข้อความ chat ที่เคยบันทึกไว้ทั้งหมด
+				if(parseInt(data[0].max_id)>parseInt(maxID)){ // เทียบว่าข้อมูล chat_id .ใหม่กว่าที่แสดงหรือไม่
+					$("#h_maxID").val(data[k].max_id); // เก็บ chat_id เป็น ค่าล่าสุด
+					// แสดงข้อความการ chat มีการประยุกต์ใช้ ตำแหน่งข้อความ เพื่อจัด css class ของข้อความที่แสดง
+					$("#messagesDiv").append("<div class=\""+data[k].data_align+"_box_chat\">"+data[k].data_msg+"</div>"); 
+					$("#messagesDiv")[0].scrollTop = $("#messagesDiv")[0].scrollHeight; // เลือน scroll ไปข้อความล่าสุด  	
+				}
+			};
+		}else{ // ถ้าเป็นข้อมูลที่เพิ่งส่งไปล่าสุด
+			if(parseInt(data[0].max_id)>parseInt(maxID)){ // เทียบว่าข้อมูล chat_id .ใหม่กว่าที่แสดงหรือไม่
+				$("#h_maxID").val(data[0].max_id); // เก็บ chat_id เป็น ค่าล่าสุด
+				// แสดงข้อความการ chat มีการประยุกต์ใช้ ตำแหน่งข้อความ เพื่อจัด css class ของข้อความที่แสดง
+				$("#messagesDiv").append("<div class=\""+data[0].data_align+"_box_chat\">"+data[0].data_msg+"</div>"); 
+				$("#messagesDiv")[0].scrollTop = $("#messagesDiv")[0].scrollHeight;   // เลือน scroll ไปข้อความล่าสุด
+			}
+		}
+		first_load++;// บวกค่า first_load
+	});		
 }
+// กำหนดให้ทำงานทกๆ 2.5 วินาทีเพิ่มแสดงข้อมูลคู่สนทนา
+setInterval(function(){
+	var userID = $("#userID2").val(); // id user ของผู้รับ
+	load_chat(userID); // เรียกใช้งานฟังก์ช่นแสดงข้อความล่าสุด
+},2500);	
+$(function(){
+	
 
+ /// เมื่อพิมพ์ข้อความ แล้วกดส่ง
+  $("#msg").keypress(function (e) { // เมื่อกดที่ ช่องข้อความ  
+	if (e.keyCode == 13) { // ถ้ากดปุ่ม enter  
+	  var user1 = $("#userID1").val(); // เก็บ id user  ผู้ใช้ที่ส่ง
+	  var user2 = $("#userID2").val(); // เก็บ id user  ผู้ใช้ที่รับ
+	  var msg = $("#msg").val();  // เก็บค่าข้อความ  
+	  $.post("ajax_chat.php",{
+		  user1:user1,
+		  user2:user2,
+		  msg:msg
+	  },function(data){
+		  	load_chat(user2);// เรียกใช้งานฟังก์ช่นแสดงข้อความล่าสุด
+	  		$("#msg").val(""); // ล้างค่าช่องข้อความ ให้พร้อมป้อนข้อความใหม่  		  
+	  });
+
+	}  
+  });  
+  
+});
 
 </script>
