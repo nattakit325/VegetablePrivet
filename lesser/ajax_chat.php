52cdb79e377e8d3837347ee4a3e69a77f100fa95
nattakit415 <nayangklukcity@gmail.com>
2018-09-03 17:59:41 +0700

    ddd

--- lesser/ajax_chat.php
+++ lesser/ajax_chat.php
@@ -6,7 +6,7 @@ include("dbconnect.php");
 if(empty($_SESSION['username'])){
 	$user = 'Nomember';
 }else{
-	$user = $_POST['user1'];
+	$user = $_SESSION['username'];
 }
 
 if(isset($_POST['user2']) && $_POST['user2']!="" 
