12c9df9aaad86762426b9b1a455be28c5e5351b0
nattakit325 <nayangklukcity@gmil.com>
2018-10-16 04:13:10 +0700

    d

--- lesser/ProductDetail.php
+++ lesser/ProductDetail.php
@@ -339,14 +339,44 @@ div#messagesDiv{
 			</div>
 			<div class="row">
 				<div class="col-md-8">
-					<div class="row">
-						<div class="col-md-12">
-							<div class="about-inner">
-								<img class="img-responsive" src="uploads_product/<?php echo $objResult["ProductPicture"]; ?>" alt="About" height="100%" width="100%">
+								
 
-							</div>
-						</div>
-					</div>
+								<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="uploads_product/P1010103.jpg" alt="Los Angeles" style="width:1000px;height: 500px">
+      </div>
+
+      <div class="item">
+        <img src="uploads_product/product.png" alt="Chicago" style="width:1000px;height: 500px">
+      </div>
+    
+      <div class="item">
+        <img src="uploads_product/กาดโจ้2477LingFern_4.jpg" alt="New york" style="width:1000px;height: 500px">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+							
+						
 				</div>
 
 				<div class="col-md-4">
