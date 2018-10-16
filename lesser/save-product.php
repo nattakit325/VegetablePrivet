a4ee127129aec7c7b1b9eb4f388f3ba03020ef88
nattakit325 <nayangklukcity@gmil.com>
2018-10-17 03:20:50 +0700

    creat img

--- lesser/save-product.php
+++ lesser/save-product.php
@@ -44,7 +44,7 @@
     		if(isset($_POST["submit"]) ) {
 
     			$check = getimagesize($_FILES["fileToUpload".$i]["tmp_name"]);
-    			
+
     		if($check !== false) {
         	
         		$uploadOk = 1;
@@ -126,7 +126,7 @@
 	$Productid=$id;
 
 	$sql="SELECT d.name as ProductName, d.picture as picture, d.detail as detail, p.name_surname as name_surname,p.address as address,
-		  p.phone as phone,p.facebook as facebook, p.line as line,p.picture as img  
+		  p.phone as phone,p.facebook as facebook, p.line as line,p.picture as img,d.picture2 as img2,d.picture3 as img3  
 		  FROM selllist s INNER join product d 
 		  on s.productid = d.id
 		  INNER JOIN profile p 
@@ -350,12 +350,40 @@
 			<div class="row">
 				<div class="col-md-8">
 					<div class="row">
-						<div class="col-md-12">
-							<div class="about-inner">
-								<img class="img-responsive" src="uploads_product/<?php echo $objResult["picture"]; ?>" alt="About" height="100%" width="100%">
-								
-							</div>
-						</div>
+							<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="uploads_product/<?php echo $objResult["picture"]; ?>" alt="Los Angeles" style="width:1000px;height: 500px">
+      </div>
+
+      <div class="item">
+        <img src="uploads_product/<?php echo $objResult["img2"]; ?>" alt="Chicago" style="width:1000px;height: 500px">
+      </div>
+    
+      <div class="item">
+        <img src="uploads_product/<?php echo $objResult["img3"]; ?>" alt="New york" style="width:1000px;height: 500px">
+      </div>
+    </div>
+
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 					</div>
 				</div>
 				
@@ -367,6 +395,7 @@
 								<ul>
 									<li>
 										<?php echo $objResult["detail"]; ?>
+
 									</li>
 									
 								</ul>
